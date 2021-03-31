79d780fda75366332045081446cffba337ee3c28
EC2 Default User <ec2-user@ip-172-31-26-195.ap-northeast-2.compute.internal>
2021-03-31 10:15:56 +0000

    add output

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "this_s3_bucket_id" {
+  value       = module.s3_bucket.this_s3_bucket_id
+}
+
+output "this_s3_bucket_region" {
+  value       = module.s3_bucket.this_s3_bucket_region
+}
+
+output "this_s3_bucket_website_domain" {
+  value       = module.s3_bucket.this_s3_bucket_website_domain
+}
